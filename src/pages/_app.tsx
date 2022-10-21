b6647da0d293a5c9c1bdbf5f594e38f65a428533
enrico pozzi <enricopozzi@MacBook-Pro-de-enrico.local>
2022-10-21 00:29:11 -0300

    added google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+     <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-M9GFFJ0QS8"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-M9GFFJ0QS8');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,18 +14,6 @@ const Home: NextPage = () => {
       <Head>
         <title>Queza | Especialistas em software</title>
       </Head>
-      <Script
-        strategy='afterInteractive'
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-M9GFFJ0QS8');
-          `
-        }}  
-        />
       <div id="home" className='lg:px-20 px-3 min-h-[100vh] flex flex-col items-center' >
         <Header />
         <Main />
